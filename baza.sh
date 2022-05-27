069c51a89aa93b202c0553a59c0ad6bdbefada56
snaake20 <shadowthedark20@gmail.com>
2022-05-27 22:29:23 +0300

    almost donee!

--- baza.sh
+++ baza.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-csv="baza.csv"
+csv="./baza.csv"
 bigReg="^[a-zA-Z]{2,},([1-9]|10),(([A-Za-z0-9]+((\.|\-|\_|\+)?[A-Za-z0-9]?)*[A-Za-z0-9]+)|[A-Za-z0-9]+)@(([A-Za-z0-9]+)+((\.|\-|\_)?([A-Za-z0-9]+)+)*)+\.([A-Za-z]{2,})+$"
 
 
 sortare(){
   clear
-  awk -F',' '{ if (NR>1) print $3 "\t" $2}' $csv | sort -n -r | head -n 3
+  note=$(awk -F',' '{ if (NR>1) print $3 $2}' $csv | sort -n -r)
+  echo $note
+  return
 }
 
 stergere(){
@@ -23,21 +25,52 @@ stergere(){
 
 editareNume(){
   read -p "noul nume: " replace
-  pozi=$1+1
-  # sed -i -e '${1} s/^[a-zA-Z]{2,}$/$replace/' $csv
-  awk -F','
+  while [[ ! "$replace" =~ ^[a-zA-Z]{2,}$ ]]
+  do
+    echo "nume invalid (cel putin 2 caractere si fara numere)"
+    read -p "Reintroduceti nume: " nume
+  done
+  id=$1
+  awk -F',' -v id="$id" -v nume="$replace" 'BEGIN { OFS = FS } $1 == id { $2 = nume }1' $csv > tmpfile && mv tmpfile "$csv"
 }
 
 editareNota(){
   read -p "noua nota: " replace
-  pozi=$1+1
-  echo $replace
-  sed -i -E -e "$pozi s/\,^([1-9]|10)\b\,/,$replace,/" $csv
+  while [[ ! $replace =~ ^([1-9]|10)$ ]]
+  do
+    echo "nota invalida"
+    read -p "Reintroduceti nota: " replace
+  done
+  id=$1
+  awk -F',' -v id="$id" -v nota="$replace" 'BEGIN { OFS = FS } $1 == id { $3 = nota }1' $csv > tmpfile && mv tmpfile "$csv"
+  # sed -i -E "s/^$id\,.\,([1-9]|10)\,.$/$replace/" $csv
+  return
 }
 
 editareMail(){
   read -p "noul mail: " replace
-  sed -i -e "$1s/\,\,\^(([A-Za-z0-9]+((\.|\-|\_|\+)?[A-Za-z0-9]?)*[A-Za-z0-9]+)|[A-Za-z0-9]+)@(([A-Za-z0-9]+)+((\.|\-|\_)?([A-Za-z0-9]+)+)*)+\.([A-Za-z]{2,})+$/$replace/" $csv
+  while [[ ! "$replace" =~ ^(([A-Za-z0-9]+((\.|\-|\_|\+)?[A-Za-z0-9]?)*[A-Za-z0-9]+)|[A-Za-z0-9]+)@(([A-Za-z0-9]+)+((\.|\-|\_)?([A-Za-z0-9]+)+)*)+\.([A-Za-z]{2,})+$ ]]
+  do
+    echo "adresa de email invalida"
+    read -p "Reintroduceti email-ul: " mail
+  done
+  id=$1
+  awk -F',' -v id="$id" -v email="$replace" 'BEGIN { OFS = FS } $1 == id { $4 = email }1' $csv > tmpfile && mv tmpfile "$csv"
+}
+
+editareTot() {
+  clear
+  id=$1
+  toBeReplaced=$2
+  read -p "Introdu campurile noi lipite urmate de ',' (fara id): " str
+    while [[ ! $str =~ $bigReg ]]
+    do
+      echo "ai introdus campurile gresit"
+      read -p "Introdu campurile noi lipite urmate de ',' (fara id)" str
+    done
+  final=$id','$str
+  echo $final
+  sed -i -e "s/$toBeReplaced/$final/" $csv
 }
 
 editare(){
@@ -45,34 +78,28 @@ editare(){
   read -p "Introduceti id-ul studentului a carui date urmeaza sa fie actualizate: " existingId
   while [[ ! $existingId =~ ^[0-9]+$ ]]
   do
-    echo "id invalida"
+    echo "id invalid"
     read -p "Reintroduceti id-ul: " nota
   done
   if [[ $(sed -n -e "/^$existingId\b/p" $csv) ]]
   then
     repl=$(sed -n -e "/^$existingId\b/p" $csv) #te am gasit hehe
     echo $repl
-    # echo -e "Introdu operatia de actualizare:\n
-    # '1' - editare nume,\n
-    # '2' - editare nota,\n
-    # '3' - editare mail,\n
-    # (*orice altceva) - exit 1"
-    # read -p "Introduceti operatia dorita: " str
-    # case "$str" in
-    # 1) editareNume $existingId;;
-    # 2) editareNota $existingId;;
-    # 3) editareMail $existingId;;
-    # *) exit 1;;
-    # esac
-    read -p "Introdu campurile noi lipite urmate de ',' (fara id): " str
-        while [[ ! $str =~ $bigReg ]]
-        do
-          echo "ai introdus campurile gresit"
-          read -p "Introdu campurile noi lipite urmate de ',' (fara id)" str
-        done
-  final=$existingId','$str
-  echo $final
-  sed -i -e "s/$repl/$final/" $csv
+    echo -e "Introdu operatia de actualizare:\n
+    '1' - editare nume,\n
+    '2' - editare nota,\n
+    '3' - editare mail,\n
+    '4' - editarea tuturor campurilor,\n
+    (*orice altceva) - exit 1"
+    read -p "Introduceti operatia dorita: " str
+    case "$str" in
+    1) editareNume $existingId;;
+    2) editareNota $existingId;;
+    3) editareMail $existingId;;
+    4) editareTot $existingId $repl;;
+    *) return;;
+    esac
+    
   else
     echo "id-ul nu exista"
     len=$(awk 'END { print NR }' $csv)
